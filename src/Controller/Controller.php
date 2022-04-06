45dd79b0b98e8883ee298ebac8cc395073663651
mikael-bth <mikmen1109@gmail.com>
2022-04-06 19:47:34 +0200

    Grunden för report sidan skapad med index, about och report sidor.

---
+++ src/Controller/Controller.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Controller extends AbstractController
+{
+    /**
+     * @Route("/", name="index", schemes={"https"})
+     */
+    public function index(): Response
+    {
+        return $this->render('index.html.twig', [
+            'title' => "Startsida",
+            'header' => "Presentation om mig",
+        ]);
+    }
+
+    /**
+     * @Route("/about", name="about", schemes={"https"})
+     */
+    public function about(): Response
+    {
+        return $this->render('about.html.twig', [
+            'title' => "Om kursen",
+            'header' => "Kursen MVC",
+        ]);
+    }
+
+    /**
+     * @Route("/report", name="report", schemes={"https"})
+     */
+    public function report(): Response
+    {
+        return $this->render('report.html.twig', [
+            'title' => "Redovisning",
+            'header' => "Redovisning för kursen MVC",
+        ]);
+    }
+}
